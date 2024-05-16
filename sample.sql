a43a965f495162c7a8d7bb1b026f7b1acbab7c97
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-16 23:45:24 +0530

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE WEBSITE;
+USE WEBSITE;
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    last_name VARCHAR(50),
+    first_name VARCHAR(50),
+    favorite_website VARCHAR(100)
+);
