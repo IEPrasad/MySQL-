9dd1730c6f2b603e856987a8496187d51a2efbd8
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-20 23:44:27 +0530

    Update details.sql

--- Practices/details.sql
+++ Practices/details.sql
@@ -1,10 +1,22 @@
--- CREATE DATABASE ERANDA;
--- USE ERANDA;
+-- -- CREATE DATABASE ERANDA;
+-- -- USE ERANDA;
 
-CREATE TABLE DETATILS (
-	ID_number INT(11) PRIMARY KEY,
+-- CREATE TABLE DETATILS (
+-- 	ID_number INT(11) PRIMARY KEY,
+--     name VARCHAR(100),
+--     age INT(3),
+--     birth_year YEAR
+-- );
+
+----- day 1---------
+
+-- USE ERANDA;
+CREATE TABLE DETAILS (
+	ID_number INT(11) PRIMARY KEY, 
     name VARCHAR(100),
     age INT(3),
-    birth_year YEAR
+    shcool VARCHAR(100)
 );
 
+
+------ day 2 --------------
