d6a07bce0b4ac82047cea7656d07cfb8541927da
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-22 00:57:06 +0530

    Update details.sql

--- Practices/details.sql
+++ Practices/details.sql
@@ -45,3 +45,28 @@
 
 -- -- View data in the customer table
 -- SELECT * FROM customer;
+
+--------- Finaly solve the problem and now I can see the table data ------ 
+------ day 4 ----------------
+
+Create database era;
+-- -- Use the specified database
+use era;
+
+-- -- Create the customer table
+CREATE TABLE customer (
+    customer_id INT(12) PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    salary INT(7),
+    department VARCHAR(40)
+);
+
+-- -- Insert data into the customer table
+INSERT INTO customer (customer_id, name, age, salary, department) VALUES
+    (1001, 'Eranda', 22, 3000000, 'ICT'),
+    (1002, 'Suhani', 21, 2000000, 'Business'),
+    (1003, 'Malki', 22, 1000000, 'Dance'),
+    (1004, 'Safwan', 22, 3000000, 'ICT');
+
+
