5cf0dabc504285706ee4f3d7418cf80a56133902
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-30 23:30:17 +0530

    Create may30.sql

---
+++ may30.sql
@@ -0,0 +1,11 @@
+create database may30;
+
+use may30;
+
+CREATE TABLE Details (
+    id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(50),
+    vilage VARCHAR(50),
+    car VARCHAR(50),
+    birth_year YEAR,
+);
