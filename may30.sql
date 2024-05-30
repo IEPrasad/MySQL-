22904a42d7e8d040d9b6c2bdbc309e4fa909d6ee
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-05-30 23:35:53 +0530

    Update may30.sql

--- may30.sql
+++ may30.sql
@@ -6,6 +6,11 @@ CREATE TABLE Details (
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(50),
     vilage VARCHAR(50),
-    car VARCHAR(50),
-    birth_year YEAR,
+    vehicle VARCHAR(50),
+    birth_date YEAR
 );
+
+INSERT INTO Details (id, name, vilage, vehicle, birth_year) VALUES
+(2001202010, 'Isuru', 'Kalubovitiyana', 'Car', '2001-10-27'),
+(1203012054, 'Isuru', 'Morawaka', 'Bike', '2001-12-10'),
+(2100304040, 'Isuru', 'Ruwankanda', 'Van', '2002-10-11');
