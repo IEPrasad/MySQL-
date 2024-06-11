0a0d77ebea8b68d872843a59b623d30f09afe3d5
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-11 22:14:49 +0530

    Update 3266-lab3-coding.sql

--- Lab-03/3266-lab3-coding.sql
+++ Lab-03/3266-lab3-coding.sql
@@ -1,45 +1,52 @@
+
+
 CREATE DATABASE CarSales;
 USE CarSales;
 
--- This is creating CarVarieties table
+-- Create all tables
 
 CREATE TABLE CarVarieties (
-	car_id VARCHAR(10) PRIMARY KEY,
-    car_name VARCHAR(50),
-    car_brand VARCHAR(50),
-    car_model VARCHAR(50),
-    car_year YEAR,
-    car_prise INT,
-    car_color VARCHAR(30)
+  car_id VARCHAR(10) PRIMARY KEY,
+  car_name VARCHAR(50),
+  car_brand VARCHAR(50),
+  car_model VARCHAR(50),
+  car_year YEAR,
+  car_price INT,
+  car_color VARCHAR(20)
 );
 
 CREATE TABLE Employees (
-	employee_id VARCHAR(10) PRIMARY KEY,
-    emp_name VARCHAR(60),
-    emp_position VARCHAR(50),
-    emp_salary INT,
-    emp_department VARCHAR(50),
-    Emp_joined_date DATE
+  employee_id VARCHAR(10) PRIMARY KEY,
+  emp_name VARCHAR(50),
+  emp_salary INT,
+  emp_department VARCHAR(50),
+  emp_joined_data DATA
 );
 
 CREATE TABLE Sales (
-	sale_id VARCHAR(10),
-    sale_date DATE,
-    sale_amount INT,
-    employee_id VARCHAR(10),
-	car_id VARCHAR(10),
-	FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
-    FOREIGN KEY (car_id) REFERENCES CarVarieties(car_id)
+  sale_id VARCHAR(10) PRIMARY KEY,
+  sale_date DATE,
+  sale_amount INT,
+  employee_id VARCHAR(10),
+  FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
+  FOREIGN KEY (car_id) REFERENCES CarVarieties(car_id)
 );
 
 CREATE TABLE Dependents (
-	dependent_id VARCHAR(10) PRIMARY KEY,
-    depen_name VARCHAR(50),
-    depen_relation VARCHAR(50),
-    depen_dob DATE,
-    deopen_gender VARCHAR(10),
-    employee_id VARCHAR(10),
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+  dependent_id VARCHAR(10) PRIMARY KEY,
+  depen_name VARCHAR(50),
+  dpen_relation VARCHAR(50),
+  depen_dop DATE,
+  depen_gender VARCHAR(10),
+  employee_id VARCHAR(10),
+  FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
 );
+
+
+-- Insert initial data into the tables
+
+
+
+
 
 
