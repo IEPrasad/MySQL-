c3e5c29788ba7470c4bab1f6ec68cc6878553773
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-06-11 22:53:53 +0530

    Update 3266-lab3-coding.sql
    Fixed so many Errors in the code !

--- Lab-03/3266-lab3-coding.sql
+++ Lab-03/3266-lab3-coding.sql
@@ -18,9 +18,10 @@ CREATE TABLE CarVarieties (
 CREATE TABLE Employees (
   employee_id VARCHAR(10) PRIMARY KEY,
   emp_name VARCHAR(50),
+  emp_position VARCHAR(50),
   emp_salary INT,
   emp_department VARCHAR(50),
-  emp_joined_data DATA
+  emp_joined_date DATE
 );
 
 CREATE TABLE Sales (
@@ -28,6 +29,7 @@ CREATE TABLE Sales (
   sale_date DATE,
   sale_amount INT,
   employee_id VARCHAR(10),
+  car_id VARCHAR(10),
   FOREIGN KEY (employee_id) REFERENCES Employees(employee_id),
   FOREIGN KEY (car_id) REFERENCES CarVarieties(car_id)
 );
@@ -35,8 +37,8 @@ CREATE TABLE Sales (
 CREATE TABLE Dependents (
   dependent_id VARCHAR(10) PRIMARY KEY,
   depen_name VARCHAR(50),
-  dpen_relation VARCHAR(50),
-  depen_dop DATE,
+  depen_relation VARCHAR(50),
+  depen_dob DATE,
   depen_gender VARCHAR(10),
   employee_id VARCHAR(10),
   FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
@@ -45,8 +47,24 @@ CREATE TABLE Dependents (
 
 -- Insert initial data into the tables
 
+INSERT INTO CarVarieties (car_id, car_name, car_brand, car_model, car_year, car_price, car_color) VALUES 
 
+('C1', 'Corolla', 'Toyota', 'Corolla', 2021, 1300000, 'Blue'),
+('C2', 'Civic', 'Honda', 'Civic', 2021, 2000000, 'Red'),
+('C3', 'Accord', 'Honda', 'Accord', 2023, 3200000, 'Black');
 
+INSERT INTO Employees (employee_id, emp_name, emp_position, emp_salary, emp_department, emp_joined_date) VALUES
+('E002', 'Johnson', 'Sales Manager', 60000, 'Sales', '2021-05-18'),
+('E005', 'Jacqueline', 'Sales Rep', 40000, 'Sales', '2022-06-01'),
+('E007', 'Michael', 'Mechanic', 50000, 'Maintenance', '2022-04-01');
 
+INSERT INTO Sales (sale_id, sale_date, sale_amount, employee_id, car_id) VALUES
+('S1', '2021-04-02', 1300000, 'E005', 'C1'),
+('S2', '2021-03-10', 2000000, 'E005', 'C2'),
+('S3', '2023-07-19', 3200000, 'E002', 'C3');
 
-
+INSERT INTO Dependents (dependent_id, depen_name, depen_relation, depen_dob, depen_gender, employee_id) VALUES
+('D1', 'Mary', 'Spouse', '1995-07-13', 'Female', 'E002'),
+('D2', 'Sarah', 'Child', '2024-03-10', 'Female', 'E002'),
+('D3', 'Sophia', 'Child', '2022-07-10', 'Female', 'E005'),
+('D4', 'David', 'Spouse', '1991-10-01', 'Male', 'E007');
