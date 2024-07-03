cfecc3d5360d28ff767d42aaecb7f030523b8fff
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-07-03 22:17:45 +0530

    Update Restrict-method.sql

--- Restrict-theory/Restrict-method.sql
+++ Restrict-theory/Restrict-method.sql
@@ -13,3 +13,39 @@ when defining foreign key constraints. IT specifies that an attempt to
 delete or update a row will be restricted if there are any dependent 
 foreign key references.
 
+3. Importance of "RESTRICT"
+
+* Data Integrity: Ensures that data integrity is maintained by 
+  preventing deletions or updates that would leave orphaned records
+  in related tables.
+
+* Error Prevention: Helps to avoid accidental deletions or updates 
+  that could corrupt the relational database structure.
+
+* Controlled Deletion/Update: Provides a way to explicitly control the 
+  behavior of deletions and updates in a relational database.
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
