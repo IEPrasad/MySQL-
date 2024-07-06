13eddb48ddf29757bbc27a66731c9a189da04169
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-07-06 22:06:40 +0530

    Update more-about-ON-DELETE-ON-UPDATE.sql

--- Restrict-theory/more-about-ON-DELETE-ON-UPDATE.sql
+++ Restrict-theory/more-about-ON-DELETE-ON-UPDATE.sql
@@ -5,7 +5,7 @@ When a row in the parent table is deleted or updated, the foreign key
 constraint determines what happens to the related rows in the child
 table.
 
-'ON DELETE' ans 'ON UPDATE' Actions
+'ON DELETE' and 'ON UPDATE' Actions
 
 1. CASCADE
 
@@ -47,10 +47,37 @@ UPDATE Departments SET department_id = 3 WHERE department_id = 2;
 -- This will also update 'Jane Smith's department_id to 3
 
 
+2. SET NULL 
 
+Action: Sets the foreign key column(s) in the child table to "NULL" when the parent 
+ro is deleted or updated.
 
+Use Case: Appropriate when the relationship should be removed but the 
+child records should remain.
 
+Example: 
 
+-- Create Child table with SET NULL 
+CREATE TABLE Employees (
+  employee_id INT PRIMARY KEY,
+  emp_name VARCHAR(50),
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES Departments(department_id) ON DELETE SET NULL UPDATE SET NULL
+);
+
+-- Insert data
+INSERT INTO Departments (department_id, department_name) 
+  VALUES (1, 'Sales'), (2, 'IT');
+INSERT INTO Employees (employee_id, emp_name, department_id) 
+  VALUES (1, 'John Doe', 1), (2, 'Jane Smith', 2);
+
+-- Delete a department
+DELETE FROM Departments WHERE department_id = 1;
+-- This will set 'John Doe's department_id to NULL
+
+-- Update a department_id 
+UPDATE Departments SET department_id = 3 WHERE department_id = 3;
+-- This will set 'Jane Smith's department_id to NULL 
 
 
 
