81eac034bc50e31c76c731671506fd339a846c1d
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-07-10 23:30:57 +0530

    Update foreign-key.sql

--- Restrict-theory/foreign-key.sql
+++ Restrict-theory/foreign-key.sql
@@ -11,3 +11,15 @@ CREATE TABLE child (
     ON DELETE RESTRICT
     ON UPDATE RESTRICT
 );
+
+-- Insert a row into the parent table
+INSERT INTO parent (id, name) VALUES (1, 'Parent 1');
+
+-- Insert a row into the child table
+INSERT INTO child (id, parent_id, name) VALUES (1, 1, 'Child 1');
+
+-- Attempt to delete the parent row (will fail due to RESTRICT)
+DELETE FROM parent WHERE id = 1; -- This will fail
+
+-- Attempt to update the parent row (will fail due to RESTRICT)
+UPDATE parent SET id = 2 WHERE id = 1; -- This will fail
